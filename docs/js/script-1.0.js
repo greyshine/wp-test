6ee4e36eacea9a382bcf5bf0b86e92b68d97a0d7
dirkschumacher <kuemmel.dss@gmx.de>
2021-08-06 23:50:15 +0200

    first steps 3.0

--- docs/js/script-1.0.js
+++ docs/js/script-1.0.js
@@ -2,6 +2,26 @@ const main = function() {
     return {
         hw() {
             return 'hello-world';
+        },
+
+        init(osmId) {
+
+            console.log('init()', osmId);
+
+            const osmDiv = document.getElementById(osmId);
+            console.log('osm', osmDiv);
+
+            var map = L.map(osmDiv);
+            L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(map);
+
+            var target = L.latLng('42.881742', '-9.271396');
+            map.setView(target, 14);
+
+            L.marker(target).addTo(map);
+
+            console.log('init.');
         }
     };
 }();
